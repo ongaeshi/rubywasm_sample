76376498cdf8b1c236def3fac487ba2c68901e08
ongaeshi <ongaeshi0621@gmail.com>
2023-02-18 15:44:14 +0900

    feat: Update

--- access_properties_from_jsobject_in_function_style/main.rb
+++ access_properties_from_jsobject_in_function_style/main.rb
@@ -1,22 +1,45 @@
-p [ 1, 2, 3..4, 'five' ]   # => "[1, 2, 3..4, \"five\"]"
-p Time.new                 # => "2008-03-08 19:43:39 +0900"
+require "js"
 
-puts "foo", "bar\n", "baz"
-puts ""    # Output only newlines
-puts       # Output only newlines
-puts nil   # Output only newlines
-puts ["oui", "non"]
+class JS::Object
+  def method_missing(sym, *args, &block)
+    ret = self[sym]
+  
+    case ret.typeof
+    when "function"
+      self.call(sym, *args, &block).to_r
+    when "undefined"
+      super
+    else
+      ret.to_r
+    end
+  end
 
-print "Hello, world!"
-print "Regexp is",/ant/
-print nil
-print "\n"
-#=> Hello, world!Regexp is(?-mix:ant)
+  def respond_to_missing?(sym, include_private)
+    return true if super
+    self[sym].typeof != "undefined"
+  end
 
-$_ = "input"
-$, = "<and>"
-$\ = "<end>\n"
-print
-print "AA","BB"
-#=> input<end>
-#=> AA<and>BB<end>
+  def to_r
+    case self.typeof
+    when "number"
+      self.to_f
+    when "string"
+      self.to_s
+    else
+      self
+    end
+  end
+end
+
+document = JS.global[:document]
+content = document.querySelector('div#content')
+
+content[:innerHTML] = <<~HTML
+  <div class="info">
+    <p>Powered by <a href="https://github.com/ruby/ruby.wasm">ruby.wasm</a> (<a href="https://ruby.github.io/ruby.wasm/">doc</a>)</p>
+    <p>#{RUBY_DESCRIPTION}</p>
+  </div>
+  </div>
+HTML
+
+p content.innerHTML
