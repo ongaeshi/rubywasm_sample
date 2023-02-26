7a5fa9455be46b04451f995aa8dd4fa5e077db5e
ongaeshi <ongaeshi0621@gmail.com>
2023-02-26 17:04:12 +0900

    feat: Remove method_missing patch

--- swissgl/main.rb
+++ swissgl/main.rb
@@ -1,42 +1,5 @@
 require "js"
 
-class JS::Object
-  def method_missing(sym, *args, &block)
-    ret = self[sym]
-
-    case ret.typeof
-    when "undefined"
-      str = sym.to_s
-      if str[-1] == "="
-        self[str.chop.to_sym] = args.first
-        return args.first
-      end
-
-      super
-    when "function"
-      self.call(sym, *args, &block).to_r
-    else
-      ret.to_r
-    end
-  end
-
-  def respond_to_missing?(sym, include_private)
-    return true if super
-    self[sym].typeof != "undefined"
-  end
-
-  def to_r
-    case self.typeof
-    when "number"
-      self.to_f
-    when "string"
-      self.to_s
-    else
-      self
-    end
-  end
-end
-
 class Hash
   def to_js
     js = JS.eval("return {}")
@@ -71,14 +34,13 @@ class SwissGL
   end
 end
 
-$JS = JS.global
-canvas = $JS.document.getElementById('c')
+canvas = JS.global[:document].getElementById('c')
 $glsl = SwissGL.new(canvas)
 
 def render(t)
   t = t / 1000; # ms to sec
   $glsl.call({t:}, "UV,cos(t*TAU),1")
-  $JS.requestAnimationFrame(->(t) { render(t.to_r) });
+  JS.global.requestAnimationFrame(->(t) { render(t.to_f) });
 end
 
 render(rand * 1000)
