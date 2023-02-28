8db77d17d4d79c358da9cc41a5405d296300cba5
ongaeshi <ongaeshi0621@gmail.com>
2023-02-28 22:32:33 +0900

    feat: Add

--- swissgl/main.rb
+++ swissgl/main.rb
@@ -67,6 +67,31 @@ end
 
 render(0)
 
+# def render(t)
+#   t = t / 1000; # ms to sec
+#   state = $glsl.call(<<-EOS, {scale:1/4, story:2})
+#   out0 = Src(I);
+#   if (out0.w == 0.0) {
+#       float v = float((I.x^I.y+100)%9==0);
+#       out0 = vec4(v,0,0,1);
+#       return;
+#   }
+#   ivec2 sz = Src_size();
+#   int x=I.x,l=(x-1+sz.x)%sz.x,r=(x+1)%sz.x;
+#   int y=I.y,d=(y-1+sz.y)%sz.y,u=(y+1)%sz.y;
+#   #define S(u,v) (Src(ivec2(u,v)).x)
+#   float nhood = S(l,y)+S(r,y)+S(x,u)+S(x,d)+S(l,u)+S(l,d)+S(r,u)+S(r,d);
+#   float v = float(nhood<3.5 && nhood>1.5 && (out0.x+nhood) > 2.5);
+#   out0 = vec4(v,0,0,1);
+# EOS
+#   fade = $glsl.call({S:state[0], Blend:'d*sa+s'}, "S(I).xxx,0.9",
+#       {size:state[0][:size], filter:'nearest'})
+#   $glsl.call({state:fade}, "state(UV).xxxx");
+#   JS.global.requestAnimationFrame(->(t) { render(t.to_f) })
+# end
+
+# render(0)
+
 # Particle life
 # K = 6 # number of particle types
 # F = $glsl.call(<<-EOS, {size:[K,K], format:'r16f'})
